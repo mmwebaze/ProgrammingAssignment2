b2ef78f4fb3f2b62497582d2962a35797edc5869
Michael Mwebaze <michael.mwebaze@gmail.com>
2015-02-18 11:08:06 -0500

    assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -2,8 +2,19 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+#library('MASS')
+tracback()
 makeCacheMatrix <- function(x = matrix()) {
+  
+  inverseOfx <- NULL
+  set <- function(y) {
+    x <<- y
+    inverseOfx <<- NULL
+  }
+  get <-  x
+  setinverse <- function(inverse) inverseOfx <<- inverse
+  getinverse <- function() inverseOfx
+  return list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 
 }
 
@@ -12,4 +23,20 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  inverseOfx <- x$getinverse()
+  if (!is.null(inverseOfx) && (x$set == x$get)) {
+        print("retreive cached inverse matrix")
+    return(inverseOfx)
+  } else {
+    inverseOfx <- solve(x$get())
+    x$setinverse(inverseOfx)
+    return(inverseOfx)
+  }
 }
+
+mrx <- matrix(c(2,4,5,6,7,9,4,2,8), nrow=3, ncol=3)
+
+cacheMatrix <- makeCacheMatrix(mrx)
+cacheSolve(cacheMatrix)
+
